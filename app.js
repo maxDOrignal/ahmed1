e15ba5bf4bf9b0083a2b655841d715ad5c7c1a91
maxDOrignal <74493102+maxDOrignal@users.noreply.github.com>
2020-12-01 06:59:32 +0300

    ء

--- app.js
+++ app.js
@@ -1,20 +1,2 @@
-const menuBar = document.querySelector('.parBars');
-const b1      = document.querySelector('.b1');
-const b2      = document.querySelector('.b2');
-const b3      = document.querySelector('.b3');
-const nav     = document.querySelector('.sec1 .header ul');
-const bod     = document.querySelector('body');
+'use strict';
 
-menuBar.addEventListener('click', function() {
-    console.log("ok");
-    b1.classList.toggle('rotateR');
-    b2.classList.toggle('hide');
-    b3.classList.toggle('rotateL');
-    nav.classList.toggle('show');
-    document.body.classList.toggle('overflowHidden')
-});
-
-
-function translate() {
-    
-}
